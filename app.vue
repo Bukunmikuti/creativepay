fbb053c00061e69d781308b6fcf3cda742f4ecd8
seyiadisa <oluwaseyiadisaa@gmail.com>
2023-04-15 18:51:40 +0100

    project setup with tailwindcss

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="underline">Hello World</h1>
+  </div>
+</template>
--- pages/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1 class="underline">Hello World</h1>
-</template>
-
-<script>
-export default {
-  name: 'IndexPage'
-}
-</script>
