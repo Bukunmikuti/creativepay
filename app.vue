40184f2776f9984474729c0b905b39e9a74fc5d9
seyiadisa <oluwaseyiadisaa@gmail.com>
2023-04-29 12:08:37 +0100

    added job opportunities section and hover effects

--- app.vue
+++ app.vue
@@ -18,7 +18,7 @@
       <p class="header-text">
         Empowering Creative Professionals to Find the Perfect Job
       </p>
-      <p class="text-lg font-bold text-center opacity-50 text-[#1E1E1E] md:mx-52">
+      <p class="text-lg font-bold text-center opacity-50 text-[#1E1E1E] md:mx-28 lg:mx-52">
         We offer a user-friendly interface and advanced search filters that allow
         you to narrow down your search and find the right job faster.
       </p>
@@ -40,7 +40,7 @@
       <button type="button" class="button">Get Started <img class="inline absolute h-5 mt-[3px] ml-1.5"
           src="~/assets/svg/btn_arrow.svg" alt="" />
       </button>
-      <img src="~/assets/images/header-image.png" alt="image showing map" class="w-full md:w-4/5 shadow-lg" />
+      <img src="~/assets/images/header-image.png" alt="image showing map" class="w-11/12 md:w-4/5 shadow-lg" />
     </header>
 
     <section id="discover" class="flex flex-col gap-8 justify-center mx-8 mt-32 items-center">
@@ -63,7 +63,7 @@
           <div class="">
             <p class="my-3 discover-card-header">
               {{ card.title }}
-              <img src="~/assets/svg/arrow.svg" alt="" class="inline">
+              <img src="~/assets/svg/arrow.svg" alt="" class="inline pl-2">
             </p>
             <p class="discover-card-text">{{ card.description }}</p>
           </div>
@@ -89,35 +89,40 @@
       <p class="header-text">
         Keep in touch with recruiters with our easy made tool
       </p>
-      <img src="~/assets/images/table_container.png" alt="" class="w-full">
+      <img src="~/assets/images/table_container.png" alt="" class="w-11/12">
     </section>
 
-    <!-- <section id="today-job" class="flex flex-col gap-8 justify-center mx-8 mt-28 px-6 py-12 items-start">
-    <h1 class="header-tag">
-      Today's Job
-    </h1>
-    <p class="header-text text-left">
-      Check today's job opportunities
-    </p>
-    <div class="flex flex-row justify-start">
-      <div class="job-card">
-        <div class="job-card-image">
-          <img src="" alt="">
-        </div>
-        <div class="job-card-header">
-          BQ LTD
-        </div>
-        <div class="job-card-text">
-          A company specializing in underwater exploration and research. They develop and use advanced technology y to
-          discover new species, study marine life, and find solutions to protect and conserve the ocean.
-        </div>
-        <div>
-          <span class="job-card-tag bg-[#6260dd26] text-[#6260DD]">3D Illustration</span>
-          <span class="job-card-tag bg-[#1C1F3226] text-[#1C1F32]">Product Design</span>
+    <section id="today-job"
+      class="flex flex-col gap-8 justify-center items-start mx-8 lg:mx-20 mt-28 px-8 lg:px-16 py-12">
+      <h1 class="header-tag">
+        Today's Job
+      </h1>
+      <div class="flex flex-row flex-wrap justify-between items-center gap-10 w-full lg:pr-10">
+        <p class="header-text">
+          Check today's job opportunities
+        </p>
+        <button type="button" class="button">Previous featured jobs <img class="inline h-5 mt-[3px] ml-1 absolute"
+            src="~/assets/svg/btn_arrow.svg" alt="" /></button>
+      </div>
+      <div class="flex flex-row flex-wrap justify-center lg:justify-between items-center gap-y-8 gap-x-8 mt-4">
+        <div v-for="card in job_cards" class="job-card px-8 py-10 rounded-md w-4/5 md:w-5/12 xl:w-1/4">
+          <div class="job-card-image mb-6">
+            <img :src="card.src" :alt="card.title + ' logo'">
+          </div>
+          <div class="job-card-header font-bold text-xl mb-3">
+            {{ card.title }}
+          </div>
+          <div class="job-card-text font-semibold text-base mb-3">
+            {{ card.description }}
+          </div>
+          <div>
+            <span v-for="tag in card.tags" class="job-card-tag" :class="`bg-[${tag.bgColor}] text-[${tag.color}]`">
+              {{ tag.title }}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  </section> -->
+    </section>
 
     <!-- <section>
     <h1 class="header-tag">
@@ -133,7 +138,7 @@
         <img src="~/assets/images/icon.png" class="inline absolute mt-0.5 w-8" alt="">
         <p class="inline ml-10 text-2xl">Creativepay</p>
       </div>
-      <div class="grid grid-flow-row grid-cols-2 md:grid-cols-4 gap-10">
+      <div class="grid grid-flow-row grid-cols-2 md:grid-cols-4 gap-8">
         <div>
           <p class="footer-header">Creativepay</p>
           <ul>
@@ -193,7 +198,12 @@ import imgUX from "~/assets/images/ux_research.png"
 import img3D from "~/assets/images/3d_designer.png"
 import imgGraphics from "~/assets/images/graphics_designer.png"
 
-
+import imgBQ from "~/assets/images/bq_ltd.png"
+import imgBeyond from "~/assets/images/beyond.png"
+import imgEcho from "~/assets/images/echo_dyn.png"
+import imgNeo from "~/assets/images/neo_health.png"
+import imgSolaris from "~/assets/images/solaris.png"
+import imgTimeKeep from "~/assets/images/time_keep.png"
 
 export default {
   data() {
@@ -245,6 +255,44 @@ export default {
           description: "specializes in creating visual content to communicate a message or idea through various mediums such as print, digital media, and branding. They work in various industries, including advertising, marketing, publishing, and entertainment.",
           src: imgGraphics,
         },
+      ],
+      job_cards: [
+        {
+          title: "BQ LTD",
+          description: "A company specializing in underwater exploration and research. They develop and use advanced technology to discover new species, study marine life, and find solutions to protect and conserve the ocean.",
+          src: imgBQ,
+          tags: [{ title: "3D Illustration", bgColor: "#6260dd26", color: "#6260DD" }, { title: "Product Design", bgColor: "#1C1F3226", color: "#1C1F32" }]
+        },
+        {
+          title: "Solaris Energy",
+          description: "Solaris Energy is a renewable energy company that specializes in solar technology. They develop and install solar panels and systems for homes, businesses, and cities, with a goal to help reduce carbon emissions",
+          src: imgSolaris,
+          tags: [{ title: "Brand Design", bgColor: "#c5460026", color: "#c54600" }]
+        },
+        {
+          title: "Time Keepers",
+          description: "TimeKeepers is a company that specializes in time-travel technology. They offer exclusive trips to different eras in history, enabling customers to witness significant events and meet historical figures firsthand.",
+          src: imgTimeKeep,
+          tags: [{ title: "Communication Design", bgColor: "#A1C8CA26", color: "#A1C8CA" }]
+        },
+        {
+          title: "Beyond",
+          description: "Beyond is a fictional company that specializes in sustainable and eco-friendly solutions for the world's water crisis. They develop and implement water filtration systems, desalination technology,and irrigation systems that support agriculture",
+          src: imgBeyond,
+          tags: [{ title: "UX Research", bgColor: "#E6913B26", color: "#C54600" }, { title: "UX Writer", bgColor: "#41A93F26", color: "#41A93F" }]
+        },
+        {
+          title: "Echo Dynamics",
+          description: "Echo Dynamics is a company that designs and manufactures advanced sound systems for music enthusiasts and professionals. Their cutting-edge technology delivers high-quality sound experiences, making their products a popular choice ",
+          src: imgEcho,
+          tags: [{ title: "Digital Marketer", bgColor: "#6260DD26", color: "#6260DD" }]
+        },
+        {
+          title: "Neo Health",
+          description: "NeoHealth is a healthcare company that focuses on the development of personalized medicine. They use genetic testing and other advanced technologies to offer patients customized treatment plans based on their unique genetic makeup, medical history, and life",
+          src: imgNeo,
+          tags: [{ title: "Product Design", bgColor: "#1C1F3226", color: "#1C1F32" }]
+        },
       ]
     }
   }
@@ -260,11 +308,18 @@ export default {
   margin: 0;
   /* overflow-x: hidden; */
   letter-spacing: -0.02em;
+  transition: 0.5s;
 }
 
 @media screen and (min-width: 768px) {
   .header-text {
-    margin: 0rem 6rem;
+    margin: 0rem 10rem;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .header-text {
+    margin: 0rem 900px;
   }
 }
 
@@ -315,6 +370,15 @@ footer li {
   background: #6260DD;
 }
 
+.button:hover {
+  background: linear-gradient(90.13deg, #6260DD -16.47%, #1C1F32 9.91%, #43437A 100.77%, rgba(98, 96, 221, 0.74) 124.22%);
+}
+
+.button:hover img {
+  content: "";
+  padding-left: 5px;
+}
+
 .header-tag {
   padding: 4px 20px;
   background: rgba(241, 241, 253, 1);
@@ -353,17 +417,15 @@ footer li {
   background: white;
 }
 
+.discover-card:focus,
 .discover-card:hover {
   outline: 2px solid rgba(67, 67, 122, 1);
   box-shadow: 0px 2px 25px 0px #1E1E1E1A;
   border-radius: 20px;
 }
 
-.discover-card:focus {
-  outline: 2px solid rgba(67, 67, 122, 1);
-  box-shadow: 0px 2px 25px 0px #1E1E1E1A;
-  border-radius: 20px;
-
+.discover-card:hover .discover-card-header>img {
+  padding-left: 15px;
 }
 
 .discover-card-image {
@@ -394,27 +456,36 @@ footer li {
   background: rgba(236, 237, 235, 1);
 }
 
+#today-job .header-text {
+  margin: 0;
+  text-align: left;
+}
+
 .job-card {
   background: white;
+  border: 0.5px solid #1E1E1E80;
+  box-shadow: 0px 1px 5px 0px #1E1E1E26;
   padding: 18px 16px;
 }
 
-.job-card-image {}
+.job-card-image {
+  width: 60px;
+}
 
 .job-card-header {
-  color: rgba(30, 30, 30, 0.8);
+  color: #1e1e1ecc;
   font-weight: 700;
   font-size: 20px;
 }
 
 .job-card-text {
-  color: rgba(30, 30, 30, 0.8);
+  color: #1e1e1ecc;
   font-weight: 600;
   font-size: 16px;
 }
 
 .job-card-tag {
-  padding: 8px 14-+px;
+  padding: 4px 12px;
   margin-right: 10px;
   border-radius: 20px;
   font-weight: 600;
